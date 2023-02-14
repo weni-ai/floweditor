2b44ae4bf975092c26b72ab0912226e0689c2b00
Paulo Bernardo <paulo.bernardo@weni.ai>
2023-02-14 16:34:37 -0300

    feat: new dummy service in lambda external services list

--- lambda/external_services.js
+++ lambda/external_services.js
@@ -4,8 +4,13 @@ const external_services = [
     uuid: '4b154b06-5ecd-43d9-afca-39738e6859d7',
     name: 'Omie dummy project',
     type: 'omie',
-    created_on: '2019-10-15T20:07:58.529130Z',
-    calls: [{ name: 'get contact' }]
+    created_on: '2019-10-15T20:07:58.529130Z'
+  },
+  {
+    uuid: '5b154b06-5ecd-43d9-afca-39738e6859d7',
+    name: 'Omie dummy project 2',
+    type: 'omie',
+    created_on: '2020-10-15T20:07:58.529130Z'
   }
 ];
 
