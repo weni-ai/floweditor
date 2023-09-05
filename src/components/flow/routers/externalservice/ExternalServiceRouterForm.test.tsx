b1490f8a13c0c9fb3fd1300e30e937d634bff933
Paulo Bernardo <paulo.bernardo@weni.ai>
2023-09-05 12:15:09 -0300

    feat: update remaining tests

--- src/components/flow/routers/externalservice/ExternalServiceRouterForm.test.tsx
+++ src/components/flow/routers/externalservice/ExternalServiceRouterForm.test.tsx
@@ -3,17 +3,10 @@ import { Types } from 'config/interfaces';
 import { RenderNode } from 'store/flowContext';
 import ExternalServiceRouterForm from './ExternalServiceRouterForm';
 import * as React from 'react';
-import {
-  render,
-  fireEvent,
-  fireChangeText,
-  fireTembaSelect,
-  fireUnnnicInputChangeText,
-  fireUnnnicSelect,
-  fireUnnnicAutocompleteSelectWithArray
-} from 'test/utils';
+import { render, fireEvent, fireUnnnicInputChangeText } from 'test/utils';
 import { act, wait } from '@testing-library/react';
 import { RouterFormProps } from '../../props';
+import userEvent from '@testing-library/user-event';
 
 const externalServiceAsset = require('test/assets/external_services.json');
 
@@ -37,16 +30,10 @@ describe(ExternalServiceRouterForm.name, () => {
       it('should render', async () => {
         externalServiceForm.assetStore.externalServices.items = {
           [externalServiceAsset.results[0].uuid]: {
-            content: externalServiceAsset.results[0],
-            name: externalServiceAsset.results[0].name,
-            type: externalServiceAsset.results[0].external_service_type,
-            id: externalServiceAsset.results[0].uuid
+            ...externalServiceAsset.results[0]
           },
           [externalServiceAsset.results[1].uuid]: {
-            content: externalServiceAsset.results[1],
-            name: externalServiceAsset.results[1].name,
-            type: externalServiceAsset.results[1].external_service_type,
-            id: externalServiceAsset.results[1].uuid
+            ...externalServiceAsset.results[1]
           }
         };
         let rendered: any;
@@ -62,16 +49,10 @@ describe(ExternalServiceRouterForm.name, () => {
       it('should save changes', async () => {
         externalServiceForm.assetStore.externalServices.items = {
           [externalServiceAsset.results[0].uuid]: {
-            content: externalServiceAsset.results[0],
-            name: externalServiceAsset.results[0].name,
-            type: externalServiceAsset.results[0].external_service_type,
-            id: externalServiceAsset.results[0].uuid
+            ...externalServiceAsset.results[0]
           },
           [externalServiceAsset.results[1].uuid]: {
-            content: externalServiceAsset.results[1],
-            name: externalServiceAsset.results[1].name,
-            type: externalServiceAsset.results[1].external_service_type,
-            id: externalServiceAsset.results[1].uuid
+            ...externalServiceAsset.results[1]
           }
         };
         let rendered: any;
@@ -81,13 +62,7 @@ describe(ExternalServiceRouterForm.name, () => {
           return undefined;
         });
 
-        await act(async () => {
-          fireUnnnicSelect(
-            rendered.getByTestId('temba_select_external_service'),
-            { value: [externalServiceAsset.results[0]] },
-            'value'
-          );
-        });
+        userEvent.click(rendered.getByText('ChatGPT dummy project'));
 
         expect(rendered.baseElement).toMatchSnapshot();
 
@@ -105,21 +80,8 @@ describe(ExternalServiceRouterForm.name, () => {
 
         fireUnnnicInputChangeText(resultName, 'My External Service Result');
 
-        await act(async () => {
-          fireUnnnicAutocompleteSelectWithArray(
-            rendered.getByTestId('temba_select_aditional_prompts'),
-            [
-              {
-                text: 'Aditional Prompt 1 content',
-                uuid: 'ab154b06-5ecd-43d9-afca-39738e6859d7'
-              },
-              {
-                text: 'Aditional Prompt 2 content',
-                uuid: 'ac154b06-5ecd-43d9-afca-39738e6859d7'
-              }
-            ]
-          );
-        });
+        userEvent.click(rendered.getByText('Aditional Prompt 1 content'));
+        userEvent.click(rendered.getByText('Aditional Prompt 2 content'));
 
         fireEvent.click(okButton);
         expect(externalServiceForm.updateRouter).toHaveBeenCalled();
@@ -145,14 +107,9 @@ describe(ExternalServiceRouterForm.name, () => {
 
     describe('render', () => {
       it('should render', async () => {
-        let rendered: any;
-        await act(async () => {
-          rendered = render(<ExternalServiceRouterForm {...externalServiceForm} />);
-          return undefined;
-        });
-
+        const { baseElement } = render(<ExternalServiceRouterForm {...externalServiceForm} />);
         await wait();
-        expect(rendered.baseElement).toMatchSnapshot();
+        expect(baseElement).toMatchSnapshot();
       });
     });
 
@@ -160,77 +117,38 @@ describe(ExternalServiceRouterForm.name, () => {
       it('should save changes', async () => {
         externalServiceForm.assetStore.externalServices.items = {
           [externalServiceAsset.results[0].uuid]: {
-            content: externalServiceAsset.results[0],
-            name: externalServiceAsset.results[0].name,
-            type: externalServiceAsset.results[0].external_service_type,
-            id: externalServiceAsset.results[0].uuid
+            ...externalServiceAsset.results[0]
           },
           [externalServiceAsset.results[1].uuid]: {
-            content: externalServiceAsset.results[1],
-            name: externalServiceAsset.results[1].name,
-            type: externalServiceAsset.results[1].external_service_type,
-            id: externalServiceAsset.results[1].uuid
+            ...externalServiceAsset.results[1]
           }
         };
-        let rendered: any;
 
-        await act(async () => {
-          rendered = render(<ExternalServiceRouterForm {...externalServiceForm} />);
-          return undefined;
-        });
+        const { baseElement, getByTestId, getAllByTestId, getByText } = render(
+          <ExternalServiceRouterForm {...externalServiceForm} />
+        );
 
         await wait();
 
-        await act(async () => {
-          fireUnnnicSelect(
-            rendered.getByTestId('temba_select_external_service'),
-            { value: [externalServiceAsset.results[1]] },
-            'value'
-          );
-        });
+        userEvent.click(getByText('Omie dummy project'));
 
-        expect(rendered.baseElement).toMatchSnapshot();
+        expect(baseElement).toMatchSnapshot();
 
-        const okButton = rendered.getByText('Ok');
-        const resultName = rendered.getByTestId('Save as result');
+        const okButton = getByText('Ok');
+        const resultName = getByTestId('Save as result');
 
         fireUnnnicInputChangeText(resultName, '');
         fireEvent.click(okButton);
         expect(externalServiceForm.updateRouter).not.toBeCalled();
 
-        await act(async () => {
-          fireUnnnicSelect(
-            rendered.getByTestId('temba_select_external_service'),
-            { value: [externalServiceAsset.results[1]] },
-            'value'
-          );
-        });
-
-        await act(async () => {
-          fireUnnnicSelect(
-            rendered.getByTestId('temba_select_external_service_call'),
-            {
-              value: [
-                {
-                  name: 'IncluirContato',
-                  value: 'IncluirContato',
-                  verboseName: 'Inserir Contato'
-                }
-              ]
-            },
-            'value'
-          );
-        });
+        userEvent.click(getByText('Inserir Contato'));
 
         await act(async () => {
           fireUnnnicInputChangeText(resultName, 'My External Service Result');
         });
 
         await act(async () => {
-          fireUnnnicInputChangeText(
-            rendered.getAllByTestId('Service Call Param Data')[0],
-            'data 1'
-          );
+          fireUnnnicInputChangeText(getAllByTestId('Service Call Param Data')[0], 'data 1');
         });
 
         fireEvent.click(okButton);
@@ -238,10 +156,7 @@ describe(ExternalServiceRouterForm.name, () => {
         expect(externalServiceForm.updateRouter).not.toBeCalled();
 
         await act(async () => {
-          fireUnnnicInputChangeText(
-            rendered.getAllByTestId('Service Call Param Data')[1],
-            'data 2'
-          );
+          fireUnnnicInputChangeText(getAllByTestId('Service Call Param Data')[1], 'data 2');
         });
 
         fireEvent.click(okButton);
