1eb926aa27dc2f07f8c8d633295acc962ad3e846
Paulo Bernardo <paulo.bernardo@weni.ai>
2024-02-09 11:11:25 -0300

    feat: content collapse component

---
+++ src/components/contentcollapse/ContentCollapse.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { applyVueInReact } from 'vuereact-combined';
+import styles from './ContentCollapse.module.scss';
+
+// @ts-ignore
+import { unnnicIcon } from '@weni/unnnic-system';
+
+const UnnnicIcon = applyVueInReact(unnnicIcon);
+
+interface ContentCollapseProps {
+  title: string;
+  children: React.ReactNode;
+  hasError?: boolean;
+  wrapper_class?: string;
+}
+
+interface ContentCollapseState {
+  isOpen: boolean;
+}
+
+export default class ContentCollapse extends React.Component<
+  ContentCollapseProps,
+  ContentCollapseState
+> {
+  constructor(props: ContentCollapseProps) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  private toggleCollapse(): void {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className={styles.collapse + ' ' + (this.props.hasError ? styles.error : '')}>
+        <div
+          data-testid={this.props.title}
+          className={styles.collapse_header}
+          onClick={() => this.toggleCollapse()}
+        >
+          <span>{this.props.title}</span>
+          <UnnnicIcon icon={this.state.isOpen ? 'expand_less' : 'expand_more'} />
+        </div>
+
+        {this.state.isOpen && <div className={this.props.wrapper_class}>{this.props.children}</div>}
+      </div>
+    );
+  }
+}
